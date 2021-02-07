8285d16eb18afb92c210f7eb6e1952b45663c31a
sufian shishir <abusufian.shishir2015@gmail.com>
2021-02-07 18:59:05 +0600

    last-commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-
-
+// searchBtn Eventlistener and loop through for innerDish
 const searchBtn = document.getElementById('search-btn').addEventListener('click', function () {
+    document.getElementById('dishIngredients').style.display = 'none';
     let innerDish = document.getElementsByClassName('food');
     for (let i = 0; i < innerDish.length; i++) {
         const dish = innerDish[i];
@@ -9,30 +9,54 @@ const searchBtn = document.getElementById('search-btn').addEventListener('click'
 
     let getMealArea = document.getElementById('search-meal').value;
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${getMealArea}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${getMealArea}`)
         .then(res => res.json())
         .then(data => displayMeals(data.meals));
 
 });
 
+// displayMeals function for display the foods in dishDiv.
 const displayMeals = foods => {
     const dishesDiv = document.getElementById('dishes');
-
-    for (let i = 0; i < foods.length; i++) {
-        const food = foods[i];
+    foods.forEach(food => {
         const dishDiv = document.createElement('div');
         dishDiv.className = 'food';
         dishDiv.innerHTML = `
-            <img src="${food.strMealThumb}">
-            <h3>${food.strMeal}</h3>
+        <img  onclick="displayIngredients('${food.strMeal}')" src="${food.strMealThumb}">
+        <h3 onclick ="displayIngredients('${food.strMeal}')">${food.strMeal}</h3>
             `
         dishesDiv.appendChild(dishDiv);
-    }
+    });
 };
 
-// document.getElementsByClassName('food').addEventListener('click', function () {
+// displayIngredients function.
+const displayIngredients = dishName =>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`)
+    .then(res => res.json())
+    .then(data => displayIngredientDetails(data.meals[0]))
+};
 
-// })
+// displayIngredientDetails function for show the details of a food.
+const displayIngredientDetails = foods => {
+    document.getElementById('dishIngredients').style.display = 'block';
+    const foodDetail = document.getElementById('dishIngredients')
+    foodDetail.innerHTML = `
+    <img src="${foods.strMealThumb}">
+    <h3>${foods.strMeal}</h3>
+
+    `
+    for (let i = 1; i < 21; i++) {
+        let Ingredient = `strIngredient` + i.toString();
+        let measure = `strMeasure` + i.toString();
+        if (foods[Ingredient]) {
+            dishIngredients.innerHTML += `
+        <p>${foods[Ingredient]}: ${foods[measure]}</p>
+        `
+        };
+
+    };
+};
+
 
 
 
